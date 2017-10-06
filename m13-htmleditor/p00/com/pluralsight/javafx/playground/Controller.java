7096d7d874191c1f5926c2fed917f414cd77f5c9
Buddha Jyothiprasad <jyothiprasadb@gmail.com>
2017-10-06 23:19:26 +0530

    m13-htmlarea basic example

---
+++ m13-htmleditor/p00/com/pluralsight/javafx/playground/Controller.java
@@ -0,0 +1,16 @@
+package com.pluralsight.javafx.playground;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller extends BaseController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
